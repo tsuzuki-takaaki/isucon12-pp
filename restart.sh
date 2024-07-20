05b7dd2be35663b443b16ea5c83ea195bf09c635
tsuzuki-takaaki <echo.tsuzuki.takaaki@gmail.com>
2024-07-20 03:02:40 +0000

    mysql etc -> /etc

--- restart.sh
+++ restart.sh
@@ -2,6 +2,9 @@
 ## nginx
 sudo cp /home/isucon/etc/nginx/nginx.conf /etc/nginx/nginx.conf
 sudo cp /home/isucon/etc/nginx/sites-enabled/isuports.conf /etc/nginx/sites-enabled/isuports.conf
+## mysqld
+sudo cp /home/isucon/etc/mysql/mysql.conf.d/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
 
 ## restart unit
 sudo systemctl restart nginx
+sudo systemctl restart mysql
