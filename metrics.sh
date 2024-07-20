8ecb86a691541be51e9cd022be93f7c2e7f73851
tsuzuki-takaaki <echo.tsuzuki.takaaki@gmail.com>
2024-07-20 03:58:10 +0000

    Add alp

--- metrics.sh
+++ metrics.sh
@@ -1,2 +1,5 @@
 ## MySQL Slow Query
 sudo pt-query-digest /var/log/mysql/mysql-slow.log --limit 15 > /home/isucon/log/slow-query.log
+
+## Nginx Slow Endpoint
+sudo alp ltsv --config /home/isucon/alp.yml --file /var/log/nginx/access.log > /home/isucon/log/alp.log
--- restart.sh
+++ restart.sh
@@ -1,10 +1,14 @@
-## cp etc -> /etc
+# cp etc -> /etc
 ## nginx
 sudo cp /home/isucon/etc/nginx/nginx.conf /etc/nginx/nginx.conf
 sudo cp /home/isucon/etc/nginx/sites-enabled/isuports.conf /etc/nginx/sites-enabled/isuports.conf
 ## mysqld
 sudo cp /home/isucon/etc/mysql/mysql.conf.d/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
 
-## restart unit
+# log clear
+sudo truncate -s 0 /var/log/nginx/access.log
+sudo truncate -s 0 /var/log/mysql/mysql-slow.log
+
+#restart unit
 sudo systemctl restart nginx
 sudo systemctl restart mysql
