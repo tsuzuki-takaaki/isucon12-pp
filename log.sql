d99301816cb061181ba33dc463de1b80ddfa0149
tsuzuki-takaaki <echo.tsuzuki.takaaki@gmail.com>
2024-07-20 08:01:35 +0000

    Harinaoshi

--- log.sql
+++ log.sql
@@ -1,4 +1,6 @@
 -- 生で貼った
 -- isuports
 create index `idx_created_at` on visit_history(`created_at`);
-create index `idx_t_c_p` on visit_history(`tenant_id`, `competition_id`, `player_id`);
+-- create index `idx_t_c_p` on visit_history(`tenant_id`, `competition_id`, `player_id`);
+-- 貼り直し
+alter table visit_history add index idx_iikanji(`tenant_id`, `competition_id`, `player_id`, `created_at`);
